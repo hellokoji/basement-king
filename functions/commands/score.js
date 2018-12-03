c9436f3fe6e618ce3dca383acad9eb017cbaafa5
Koji Lopez <heykoji@heykoji-macbookpro.roam.corp.google.com>
2018-12-03 08:50:26 -0800

    add scoreboard command in slack

---
+++ functions/commands/score.js
@@ -0,0 +1,33 @@
+const lib = require('lib')({token: process.env.STDLIB_TOKEN});
+const Scoreboard = require('../../utils/scoreboard.js');
+
+/**
+* /score
+*
+*   What's the score? command. Echoes to the channel what the score is
+*   back to the channel. Spits out the player name, username, and current
+*   points.
+*
+*
+* @param {string} user The user id of the user that invoked this command (name is usable as well)
+* @param {string} channel The channel id the command was executed in (name is usable as well)
+* @param {string} text The text contents of the command
+* @param {object} command The full Slack command object
+* @param {string} botToken The bot token for the Slack bot you have activated
+* @returns {object}
+*/
+module.exports = (user, channel, text = '', command = {}, botToken = null, callback) => {
+  Scoreboard.getScoreboard(players => {
+    let out = '';
+    players.forEach(element => {
+      out += element.username + ' : ' + element.points + '\n';
+    });
+    callback(null, {
+      text: out,
+      attachments: [
+        // You can customize your messages with attachments.
+        // See https://api.slack.com/docs/message-attachments for more info.
+      ]
+    });
+  });
+};
