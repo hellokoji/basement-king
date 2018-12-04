6ededed9316631c7bb3ead33365ae1ffd628dc5c
Koji Lopez <heykoji@heykoji-macbookpro.roam.corp.google.com>
2018-12-04 15:11:14 -0800

    add ping command

---
+++ functions/commands/ping.js
@@ -0,0 +1,15 @@
+/**
+* /ping
+*
+*   Quick command to ping the Basement Constable for service.
+*
+* @param {string} user The user id of the user that invoked this command (name is usable as well)
+* @param {string} channel The channel id the command was executed in (name is usable as well)
+* @param {string} text The text contents of the command
+* @param {object} command The full Slack command object
+* @param {string} botToken The bot token for the Slack bot you have activated
+* @returns {object}
+*/
+module.exports = (user, channel, text = '', command = {}, botToken = null, callback) => {
+  callback(null, { text: 'pong' });
+};
